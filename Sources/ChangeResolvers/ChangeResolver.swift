e9ec4feb4d11545a6b7a02e064522cbb4d7ad42a
Christopher Prince <chris@SpasticMuffin.biz>
2020-07-26 09:27:16 -0600

    Put generic apply method in WholeFileReplacer.

--- Sources/ChangeResolvers/ChangeResolver.swift
+++ Sources/ChangeResolvers/ChangeResolver.swift
@@ -9,13 +9,9 @@ import Foundation
 import ServerAccount
 import ServerShared
 
-public protocol ChangeResolver {
-    // This must be unique across all ChangeResolvers registered with the server.
-    static var changeResolverName: String { get }
-}
-
-public enum ChangeResolverConstants {
-    public static let maxChangeResolverNameLength = 100
+public struct ApplyResult {
+    public let newFileVersion: FileVersionInt
+    public let checkSum: String
 }
 
 public protocol ChangeResolverContents {
@@ -23,8 +19,17 @@ public protocol ChangeResolverContents {
     var uploadContents: Data? { get }
 }
 
-public extension ChangeResolver {
+public protocol ChangeResolver {
+    // This must be unique across all ChangeResolvers registered with the server.
+    static var changeResolverName: String { get }
+    
     // Apply the change resolver
-    static func apply(changes: [ChangeResolverContents], toFileUUID fileUUID: String, currentFileVersion: FileVersionInt, deviceUUID: String, cloudStorage: CloudStorage, options: CloudStorageFileNameOptions) {
-    }
+    static func apply(changes: [ChangeResolverContents], toFileUUID fileUUID: String, currentFileVersion: FileVersionInt, deviceUUID: String, cloudStorage: CloudStorage, options: CloudStorageFileNameOptions, completion: ((Swift.Result<ApplyResult, Error>) -> ())?)
 }
+
+public enum ChangeResolverConstants {
+    public static let maxChangeResolverNameLength = 100
+}
+
+
+
--- Sources/ChangeResolvers/WholeFileReplacer.swift
+++ Sources/ChangeResolvers/WholeFileReplacer.swift
@@ -1,5 +1,8 @@
 
 import Foundation
+import ServerAccount
+import ServerShared
+import LoggerAPI
 
 // Enabling the application of changes to a single file. The file must be read in its entirety from storage, some series of changes applied, and then the updates written back to storage. It is called `WholeFileReplacer` due to this mode of full read/full write to make changes.
 
@@ -13,4 +16,69 @@ public protocol WholeFileReplacer: ChangeResolver {
     
     // Convert the entire file into a Data object
     func getData() throws -> Data
+}
+
+private enum Errors: Swift.Error {
+    case downloadError(DownloadResult)
+    case failedInitializing
+    case noContentsForChange
+    case failedAddingChange(Swift.Error)
+    case failedGettingReplacerData
+    case failedUploadingNewFileVersion
+}
+
+public extension WholeFileReplacer {
+    static func apply(changes: [ChangeResolverContents], toFileUUID fileUUID: String, currentFileVersion: FileVersionInt, deviceUUID: String, cloudStorage: CloudStorage, options: CloudStorageFileNameOptions, completion: ((Swift.Result<ApplyResult, Error>) -> ())? = nil) {
+        
+        // We're applying changes and creating the next version of the file
+        let nextVersion = currentFileVersion + 1
+        
+        let currentCloudFileName = Filename.inCloud(deviceUUID:deviceUUID, fileUUID: fileUUID, mimeType:options.mimeType, fileVersion: currentFileVersion)
+        
+        Log.debug("downloadFile: \(currentCloudFileName)")
+        cloudStorage.downloadFile(cloudFileName: currentCloudFileName, options: options) { downloadResult in
+            guard case .success(data: let fileContents, checkSum: _) = downloadResult else {
+                completion?(.failure(Errors.downloadError(downloadResult)))
+                return
+            }
+            
+            guard var replacer = try? Self.init(with: fileContents) else {
+                completion?(.failure(Errors.failedInitializing))
+                return
+            }
+            
+            for change in changes {
+                guard let changeData = change.uploadContents else {
+                    completion?(.failure(Errors.noContentsForChange))
+                    return
+                }
+                
+                do {
+                    try replacer.add(newRecord: changeData)
+                } catch let error {
+                    completion?(.failure(Errors.failedAddingChange(error)))
+                    return
+                }
+            }
+            
+            guard let replacementFileContents = try? replacer.getData() else {
+                completion?(.failure(Errors.failedGettingReplacerData))
+                return
+            }
+            
+            let nextCloudFileName = Filename.inCloud(deviceUUID:deviceUUID, fileUUID: fileUUID, mimeType:options.mimeType, fileVersion: nextVersion)
+
+            Log.debug("uploadFile: \(nextCloudFileName)")
+
+            cloudStorage.uploadFile(cloudFileName: nextCloudFileName, data: replacementFileContents, options: options) { uploadResult in
+                guard case .success(let checkSum) = uploadResult else {
+                    completion?(.failure(Errors.failedUploadingNewFileVersion))
+                    return
+                }
+                
+                let result = ApplyResult(newFileVersion: nextVersion, checkSum: checkSum)
+                completion?(.success(result))
+            }
+        }
+    }
 }
