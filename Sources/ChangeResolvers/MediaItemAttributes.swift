9f2c122f1c7e39a02459be778dbf9cca3bd69870
Christopher Prince <chris@SpasticMuffin.biz>
2021-05-31 22:57:17 -0600

    version 0.2.0

---
+++ Sources/ChangeResolvers/MediaItemAttributes.swift
@@ -0,0 +1,208 @@
+//
+//  MediaItemAttributes.swift
+//  
+//
+//  Created by Christopher G Prince on 5/31/21.
+//
+
+// Each media item is going to have a file describing attributes:
+//  https://github.com/SyncServerII/Neebla/issues/15
+
+import Foundation
+ 
+public enum KeyType {
+    case unreadCount
+    case keyword
+    case badge
+}
+ 
+// Each case here must correspond to one KeyType case
+public enum KeyValue: Codable, Equatable {
+    case unreadCount(userId: String, unreadCount: Int?)
+    case keyword(String, used:Bool?)
+    case badge(userId: String, code: String?)
+    
+    enum CodingKeys: CodingKey {
+        case unreadCount, keyword, badge
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let key = container.allKeys.first
+        
+        switch key {
+        case .unreadCount:
+            var nestedContainer = try container.nestedUnkeyedContainer(forKey: .unreadCount)
+            let userId = try nestedContainer.decode(String.self)
+            let unreadCount = try nestedContainer.decode(Int?.self)
+            self = .unreadCount(userId: userId, unreadCount: unreadCount)
+            
+        case .keyword:
+            var nestedContainer = try container.nestedUnkeyedContainer(forKey: .keyword)
+            let keyword = try nestedContainer.decode(String.self)
+            let used = try nestedContainer.decode(Bool?.self)
+            self = .keyword(keyword, used: used)
+            
+        case .badge:
+            var nestedContainer = try container.nestedUnkeyedContainer(forKey: .badge)
+            let userId = try nestedContainer.decode(String.self)
+            let code = try nestedContainer.decode(String?.self)
+            self = .badge(userId: userId, code: code)
+
+        default:
+            throw DecodingError.dataCorrupted(
+                DecodingError.Context(
+                    codingPath: container.codingPath,
+                    debugDescription: "Unabled to decode enum."
+                )
+            )
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        switch self {
+        case .unreadCount(userId: let userId, unreadCount: let unreadCount):
+            var nestedContainer = container.nestedUnkeyedContainer(forKey: .unreadCount)
+            try nestedContainer.encode(userId)
+            try nestedContainer.encode(unreadCount)
+        case .keyword(let keyword, used: let used):
+            var nestedContainer = container.nestedUnkeyedContainer(forKey: .keyword)
+            try nestedContainer.encode(keyword)
+            try nestedContainer.encode(used)
+        case .badge(userId: let userId, code: let code):
+            var nestedContainer = container.nestedUnkeyedContainer(forKey: .badge)
+            try nestedContainer.encode(userId)
+            try nestedContainer.encode(code)
+        }
+    }
+}
+ 
+class KeyValues<KEY: Hashable & Codable, VALUE: Codable>: Codable {
+    var contents = [KEY: VALUE]()
+    
+    func add(key: KEY, value: VALUE) {
+        contents[key] = value
+    }
+    
+    func get(key: KEY) -> VALUE? {
+        return contents[key]
+    }
+}
+
+class UnreadCounts: KeyValues<String, Int> {
+    // For UnreadCounts we don't want just a simple replace last value strategy.
+    // Instead, I want: The maximum of two (same) userId keys should be used. I.e., the maximum across the current and last value for the same UserId.
+    // See https://github.com/SyncServerII/Neebla/issues/15#issuecomment-850734982
+    override func add(key: String, value: Int) {
+        if let lastValue = get(key: key) {
+            super.add(key: key, value: max(lastValue, value))
+        }
+        else {
+            super.add(key: key, value: value)
+        }
+    }
+}
+
+public class MediaItemAttributes: WholeFileReplacer, Codable {
+    public static var changeResolverName: String = "MediaItemAttributes"
+    
+    // Key: userId; Value: unreadCount
+    var unreadCounts = UnreadCounts()
+    
+    // Key: Keyword; Value: used/unused
+    var keywords = KeyValues<String, Bool>()
+    
+    // Key: userId; Value: Badge code
+    var badges = KeyValues<String, String>()
+    
+    public required init(with data: Data) throws {
+        let decoder = JSONDecoder()
+        let selfObject = try decoder.decode(Self.self, from: data)
+        unreadCounts = selfObject.unreadCounts
+        keywords = selfObject.keywords
+        badges = selfObject.badges
+    }
+    
+    // a `newRecord` must be an encoded `KeyValue`
+    public func add(newRecord: Data) throws {
+        let decoder = JSONDecoder()
+        
+        let keyValue = try decoder.decode(KeyValue.self, from: newRecord)
+        try add(keyValue: keyValue)
+    }
+    
+    public func getData() throws -> Data {
+        let encoder = JSONEncoder()
+        return try encoder.encode(self)
+    }
+    
+    // a `uploadContents` must be an encoded `KeyValue`
+    public static func valid(uploadContents: Data) -> Bool {
+        let decoder = JSONDecoder()
+
+        do {
+            let _ = try decoder.decode(KeyValue.self, from: uploadContents)
+            return true
+        } catch let error {
+            logger?.error("valid: \(error)")
+            return false
+        }
+    }
+    
+    enum MediaItemAttributesError: Error {
+        case nilValue
+    }
+    
+    // MARK: Class specific methods
+    
+    // For testing.
+    init() {
+    }
+    
+    // Convenience for encoding
+    public static func encode(keyValue: KeyValue) throws -> Data {
+        let encoder = JSONEncoder()
+        return try encoder.encode(keyValue)
+    }
+    
+    // The value in the KeyValue cannot be nil.
+    public func add(keyValue: KeyValue) throws {
+        switch keyValue {
+        case .unreadCount(userId: let userId, unreadCount: let unreadCount):
+            guard let unreadCount = unreadCount else {
+                throw MediaItemAttributesError.nilValue
+            }
+            self.unreadCounts.add(key: userId, value: unreadCount)
+            
+        case .keyword(let keyword, used: let used):
+            guard let used = used else {
+                throw MediaItemAttributesError.nilValue
+            }
+            self.keywords.add(key: keyword, value: used)
+            
+        case .badge(userId: let userId, code: let code):
+            guard let code = code else {
+                throw MediaItemAttributesError.nilValue
+            }
+            self.badges.add(key: userId, value: code)
+        }
+    }
+    
+    public func get(type: KeyType, key: String) -> KeyValue {
+        switch type {
+        case .unreadCount:
+            let value = self.unreadCounts.get(key: key)
+            return .unreadCount(userId: key, unreadCount: value)
+            
+        case .keyword:
+            let value = self.keywords.get(key: key)
+            return .keyword(key, used: value)
+            
+        case .badge:
+            let value = self.badges.get(key: key)
+            return .badge(userId: key, code: value)
+        }
+    }
+}
---
+++ Tests/ChangeResolversTests/MediaItemAttributesTests.swift
@@ -0,0 +1,188 @@
+//
+//  MediaItemAttributesTests.swift
+//  ChangeResolversTests
+//
+//  Created by Christopher G Prince on 5/31/21.
+//
+
+import XCTest
+@testable import ChangeResolvers
+
+class MediaItemAttributesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    // MARK: Test KeyValue coding.
+    
+    func testKeyValueCoding_keyword() throws {
+        let encoder = JSONEncoder()
+        let decoder = JSONDecoder()
+        var data: Data!
+        
+        let keyValue1 = KeyValue.keyword("Key1", used: true)
+        data = try encoder.encode(keyValue1)
+        let keyValue1Decoded = try decoder.decode(KeyValue.self, from: data)
+        XCTAssert(keyValue1 == keyValue1Decoded)
+        
+        let keyValue2 = KeyValue.keyword("Key1", used: nil)
+        data = try encoder.encode(keyValue2)
+        let keyValue2Decoded = try decoder.decode(KeyValue.self, from: data)
+        XCTAssert(keyValue2 == keyValue2Decoded)
+    }
+    
+    func testKeyValueCoding_unreadCount() throws {
+        let encoder = JSONEncoder()
+        let decoder = JSONDecoder()
+        var data: Data!
+        
+        let keyValue1 = KeyValue.unreadCount(userId: "Foo", unreadCount: 100)
+        data = try encoder.encode(keyValue1)
+        let keyValue1Decoded = try decoder.decode(KeyValue.self, from: data)
+        XCTAssert(keyValue1 == keyValue1Decoded)
+        
+        let keyValue2 = KeyValue.unreadCount(userId: "Foo", unreadCount: nil)
+        data = try encoder.encode(keyValue2)
+        let keyValue2Decoded = try decoder.decode(KeyValue.self, from: data)
+        XCTAssert(keyValue2 == keyValue2Decoded)
+    }
+    
+   func testKeyValueCoding_badge() throws {
+        let encoder = JSONEncoder()
+        let decoder = JSONDecoder()
+        var data: Data!
+        
+        let keyValue1 = KeyValue.badge(userId: "Foo", code: "badge1")
+        data = try encoder.encode(keyValue1)
+        let keyValue1Decoded = try decoder.decode(KeyValue.self, from: data)
+        XCTAssert(keyValue1 == keyValue1Decoded)
+        
+        let keyValue2 = KeyValue.badge(userId: "Foo", code: nil)
+        data = try encoder.encode(keyValue2)
+        let keyValue2Decoded = try decoder.decode(KeyValue.self, from: data)
+        XCTAssert(keyValue2 == keyValue2Decoded)
+    }
+    
+    // MARK: Test add KeyValue
+    
+    func testAddKeyValue_badge_nonNilCode() throws {
+        let mia = MediaItemAttributes()
+        let keyValue: KeyValue = .badge(userId: "Foo", code: "Bar")
+        try mia.add(keyValue: keyValue)
+        let keyValueResult = mia.get(type: .badge, key: "Foo")
+        XCTAssert(keyValue == keyValueResult)
+    }
+    
+    func testAddKeyValue_badge_nilCode() throws {
+        let mia = MediaItemAttributes()
+        let keyValue: KeyValue = .badge(userId: "Foo", code: nil)
+        do {
+            try mia.add(keyValue: keyValue)
+        } catch {
+            return
+        }
+        XCTFail()
+    }
+    
+    func testAddKeyValue_unreadCount_nonNilCode() throws {
+        let mia = MediaItemAttributes()
+        let keyValue: KeyValue = .unreadCount(userId: "Foo", unreadCount: 20)
+        try mia.add(keyValue: keyValue)
+        let keyValueResult = mia.get(type: .unreadCount, key: "Foo")
+        XCTAssert(keyValue == keyValueResult)
+    }
+    
+    func testAddKeyValue_unreadCount_nilCode() throws {
+        let mia = MediaItemAttributes()
+        let keyValue: KeyValue = .unreadCount(userId: "Foo", unreadCount: nil)
+        do {
+            try mia.add(keyValue: keyValue)
+        } catch {
+            return
+        }
+        XCTFail()
+    }
+    
+    func testAddKeyValue_keyword_nonNilCode() throws {
+        let mia = MediaItemAttributes()
+        let keyValue: KeyValue = .keyword("Foo", used: true)
+        try mia.add(keyValue: keyValue)
+        let keyValueResult = mia.get(type: .keyword, key: "Foo")
+        XCTAssert(keyValue == keyValueResult)
+    }
+    
+    func testAddKeyValue_keyword_nilCode() throws {
+        let mia = MediaItemAttributes()
+        let keyValue: KeyValue = .keyword("Foo", used: nil)
+        do {
+            try mia.add(keyValue: keyValue)
+        } catch {
+            return
+        }
+        XCTFail()
+    }
+    
+    func testAddNewRecord_badge() throws {
+        let coder = JSONEncoder()
+        let keyValue: KeyValue = .badge(userId: "Foo", code: "20")
+        let data = try coder.encode(keyValue)
+        
+        let mia = MediaItemAttributes()
+        try mia.add(newRecord: data)
+        
+//        let data2 = try coder.encode(mia)
+//        let str = String(data: data2, encoding: .utf8)
+//        print("\(String(describing: str))")
+    }
+    
+    func testAddNewRecord_unreadCount() throws {
+        let coder = JSONEncoder()
+        let keyValue: KeyValue = .unreadCount(userId: "Foo", unreadCount: 208)
+        let data = try coder.encode(keyValue)
+        
+        let mia = MediaItemAttributes()
+        try mia.add(newRecord: data)
+    }
+    
+    func testAddNewRecord_keyword() throws {
+        let coder = JSONEncoder()
+        let keyValue: KeyValue = .keyword("Bar", used: false)
+        let data = try coder.encode(keyValue)
+        
+        let mia = MediaItemAttributes()
+        try mia.add(newRecord: data)
+    }
+    
+    // MARK: Init from data
+    
+    func testInitFromData() throws {
+        let coder = JSONEncoder()
+        let keyValue: KeyValue = .badge(userId: "Foo", code: "20")
+        let data = try coder.encode(keyValue)
+        
+        let mia = MediaItemAttributes()
+        try mia.add(newRecord: data)
+        
+        let miaData = try coder.encode(mia)
+        
+        let mia2 = try MediaItemAttributes(with: miaData)
+        XCTAssert(mia2.get(type: .badge, key: "Foo") == keyValue)
+    }
+    
+    func testValidTrue() throws {
+        let coder = JSONEncoder()
+        let keyValue: KeyValue = .badge(userId: "Foo", code: "20")
+        let data = try coder.encode(keyValue)
+        
+        XCTAssert(MediaItemAttributes.valid(uploadContents: data))
+    }
+    
+    func testValidFalse() {
+        XCTAssert(!MediaItemAttributes.valid(uploadContents: Data()))
+    }
+}
